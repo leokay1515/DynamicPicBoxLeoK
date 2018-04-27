a244950d271876a0763e1ab3e319a91466816466
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2018-04-27 12:45:30 -0400

    Initial commit

---
+++ DynamicPicBoxLeoKForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DynamicPicBoxLeoK
+{
+    partial class frmDynamicPicBoxLeoK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblRequest = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(310, 256);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(87, 37);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblRequest
+            // 
+            this.lblRequest.AutoSize = true;
+            this.lblRequest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRequest.Location = new System.Drawing.Point(176, 9);
+            this.lblRequest.Name = "lblRequest";
+            this.lblRequest.Size = new System.Drawing.Size(364, 20);
+            this.lblRequest.TabIndex = 1;
+            this.lblRequest.Text = "Click on a picture box to make the man walk:";
+            // 
+            // frmDynamicPicBoxLeoK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 569);
+            this.Controls.Add(this.lblRequest);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmDynamicPicBoxLeoK";
+            this.Text = "Dynamic Picture Boxes and Click Events by Leo Kay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblRequest;
+    }
+}
+
---
+++ DynamicPicBoxLeoKForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//threading
+using System.Threading;
+
+namespace DynamicPicBoxLeoK
+{
+    public partial class frmDynamicPicBoxLeoK : Form
+    {
+        public frmDynamicPicBoxLeoK()
+        {
+            InitializeComponent();
+            this.lblRequest.Hide();
+        }
+
+        private void GeneratePictureBoxes()
+        {
+            //generate the picture boxes on the form
+            GeneratePictureBox(61, 78);
+            GeneratePictureBox(464, 78);
+            GeneratePictureBox(61, 358);
+            GeneratePictureBox(464, 358);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //show the request label
+            this.lblRequest.Show();
+
+            //generate the picture boxes
+            GeneratePictureBoxes();
+        }
+
+        private void GeneratePictureBox(int x, int y)
+        {
+            //dynamically generate a new picture box and a point at the given location (x,y)
+            PictureBox tmpPicMan = new PictureBox();
+            Point pntPic = new System.Drawing.Point(x, y);
+
+            //set the location of the picture box
+            tmpPicMan.Location = pntPic;
+
+            //set the image of the picture box
+            tmpPicMan.Image = Properties.Resources.walk1;
+
+            //stretch the image to the size of the picture box
+            tmpPicMan.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            //make the picture box width and height the same as the image
+            tmpPicMan.ClientSize = new Size(Properties.Resources.walk1.Width, Properties.Resources.walk1.Height);
+
+            //add the event listener
+            tmpPicMan.Click += new System.EventHandler(PictureBox_Click);
+
+            //add the picture box to the form
+            this.Controls.Add(tmpPicMan);
+        }
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            //cast the sender object into a picture box
+            PictureBox tmpPicMan = (PictureBox)sender;
+
+            //write a line in console to see if it was clicked
+            Console.WriteLine("Picture Box (" + tmpPicMan.Location.X + ", " + tmpPicMan.Location.Y + ") was clicked.");
+
+            //declare local variables and constants
+            const byte MAX_FRAMES = 10;
+            int pictureFrameCounter = 1;
+
+            //make the man walk
+            while (pictureFrameCounter < MAX_FRAMES + 1)
+            {
+                if (pictureFrameCounter == 1)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk1;
+                }
+                else if (pictureFrameCounter == 2)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk2;
+                }
+                else if (pictureFrameCounter == 3)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk3;
+                }
+                else if (pictureFrameCounter == 4)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk4;
+                }
+                else if (pictureFrameCounter == 5)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk5;
+                }
+                else if (pictureFrameCounter == 6)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk6;
+                }
+                else if (pictureFrameCounter == 7)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk7;
+                }
+                else if (pictureFrameCounter == 8)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk8;
+                }
+                else if (pictureFrameCounter == 9)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk9;
+                }
+                else if (pictureFrameCounter == 10)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk10;
+                }
+                //increment the counter
+                pictureFrameCounter++;
+
+                //refresh the screen
+                this.Refresh();
+
+                //pause the loop
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
